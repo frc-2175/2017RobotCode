ba3580a3a97cb2eed03db1af9d9664d81f68d1c6
Jeff Jensen <jjensen@apache.org>
2017-01-13 20:58:28 -0600

    Create first properties skeleton test classes with TODOs

---
+++ test/org/usfirst/frc/team2175/properties/JoystickPropertiesTest.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team2175.properties;
+
+import org.junit.Test;
+
+public class JoystickPropertiesTest {
+    @Test
+    public void test() {
+        // TODO write me
+    }
+}
