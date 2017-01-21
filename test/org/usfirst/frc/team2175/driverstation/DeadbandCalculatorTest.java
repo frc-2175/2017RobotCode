2f3abdeb67c3d670ab4baee83fd6c1331386b884
Max Haland <max@haland.org>
2017-01-21 13:58:54 -0600

    Add test class for DeadbandCalculator

---
+++ test/org/usfirst/frc/team2175/driverstation/DeadbandCalculatorTest.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team2175.driverstation;
+
+import org.junit.Test;
+import org.usfirst.frc.team2175.TestBase;
+
+public class DeadbandCalculatorTest extends TestBase {
+
+	@Test
+	public void testCalcLinearOutputSlope() {
+		/* Write this */
+	}
+
+	@Test
+	public void testCalcLinearOutput() {
+		/* Write this */
+	}
+
+	@Test
+	public void testCalcDeadbandedOutput() {
+		/* Write this */
+	}
+}
