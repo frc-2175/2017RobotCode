7c1c703a2243ba84dbcfcf0c5cdca4340627b4c7
Adisesh Viruru <adisesh.viruru@gmail.com>
2017-01-21 20:31:24 -0600

    change format

--- src/org/usfirst/frc/team2175/subsystem/ShooterSubsystem.java
+++ src/org/usfirst/frc/team2175/subsystem/ShooterSubsystem.java
@@ -5,23 +5,27 @@ import org.usfirst.frc.team2175.properties.WiringProperties;
 
 import com.ctre.CANTalon;
 
-public class ShooterSubsystem extends BaseSubsystem
-{
+public class ShooterSubsystem extends BaseSubsystem {
     private final CANTalon shooterMotor;
     private final CANTalon feederMotor;
     private final CANTalon agitatorMotor;
 
-    public ShooterSubsystem() { 
-	super();
-	WiringProperties wiringProperties = ServiceLocator.get(WiringProperties.class);
-	shooterMotor = new CANTalon(wiringProperties.getShooterShooterMotorDeviceNumber());
-	feederMotor = new CANTalon(wiringProperties.getShooterFeederMotorDeviceNumber());
-	agitatorMotor = new CANTalon(wiringProperties.getShooterAgitatorMotorDeviceNumber());
-   }
-    
-    public void setMotorSpeed(double shooterSpeed, double feederSpeed, double agitatorSpeed ) {
-	shooterMotor.set(shooterSpeed);
-	feederMotor.set(feederSpeed);
-	agitatorMotor.set(agitatorSpeed);
+    public ShooterSubsystem() {
+        super();
+        WiringProperties wiringProperties =
+                ServiceLocator.get(WiringProperties.class);
+        shooterMotor = new CANTalon(
+                wiringProperties.getShooterShooterMotorDeviceNumber());
+        feederMotor = new CANTalon(
+                wiringProperties.getShooterFeederMotorDeviceNumber());
+        agitatorMotor = new CANTalon(
+                wiringProperties.getShooterAgitatorMotorDeviceNumber());
+    }
+
+    public void setMotorSpeed(double shooterSpeed, double feederSpeed,
+            double agitatorSpeed) {
+        shooterMotor.set(shooterSpeed);
+        feederMotor.set(feederSpeed);
+        agitatorMotor.set(agitatorSpeed);
     }
 }
