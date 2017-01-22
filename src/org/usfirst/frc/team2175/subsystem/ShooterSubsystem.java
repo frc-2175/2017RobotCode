d3688cf2852732a50dd179aaf66171ec3c21b93b
Max Haland <max@haland.org>
2017-01-21 18:11:24 -0600

    Update format to match project standard

--- src/org/usfirst/frc/team2175/subsystem/ShooterSubsystem.java
+++ src/org/usfirst/frc/team2175/subsystem/ShooterSubsystem.java
@@ -5,18 +5,20 @@ import org.usfirst.frc.team2175.properties.WiringProperties;
 
 import com.ctre.CANTalon;
 
-public class ShooterSubsystem extends BaseSubsystem
-{
-    CANTalon shooterMotor;
-    CANTalon feederMotor;
-    CANTalon agitatorMotor;
+public class ShooterSubsystem extends BaseSubsystem {
+	CANTalon shooterMotor;
+	CANTalon feederMotor;
+	CANTalon agitatorMotor;
 
-    public ShooterSubsystem() {
-	WiringProperties wiringProperties = ServiceLocator.get(WiringProperties.class);
-	shooterMotor = new CANTalon(wiringProperties.getShooterShooterMotorDeviceNumber());
-	feederMotor = new CANTalon(wiringProperties.getShooterFeederMotorDeviceNumber());
-	agitatorMotor = new CANTalon(wiringProperties.getShooterAgitatorMotorDeviceNumber());
-
-    }
+	public ShooterSubsystem() {
+		WiringProperties wiringProperties =
+				ServiceLocator.get(WiringProperties.class);
+		shooterMotor = new CANTalon(
+				wiringProperties.getShooterShooterMotorDeviceNumber());
+		feederMotor = new CANTalon(
+				wiringProperties.getShooterFeederMotorDeviceNumber());
+		agitatorMotor = new CANTalon(
+				wiringProperties.getShooterAgitatorMotorDeviceNumber());
+	}
 
 }
