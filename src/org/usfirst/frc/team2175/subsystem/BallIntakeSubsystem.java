57b6ee4ba4504890fedc0ef3ed2adcf4f185016c
Noah Connors <noahconnors@mnmsa.org>
2017-01-21 18:02:31 -0600

    Add Ball Intake Subsystem

---
+++ src/org/usfirst/frc/team2175/subsystem/BallIntakeSubsystem.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team2175.subsystem;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.properties.WiringProperties;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public class BallIntakeSubsystem extends BaseSubsystem {
+    private final CANTalon mainMotor;
+    private final DoubleSolenoid actuateSolenoidInAndOut;
+
+    public BallIntakeSubsystem() {
+        WiringProperties wiringProperties =
+                ServiceLocator.get(WiringProperties.class);
+        mainMotor = new CANTalon(
+                wiringProperties.getBallIntakeMainMotorDeviceNumber());
+        actuateSolenoidInAndOut = new DoubleSolenoid(
+                wiringProperties.getBallIntakeActuaterForwardNumber(),
+                wiringProperties.getBallIntakeActuaterReverseNumber());
+    }
+
+    public void actuateOut() {
+        actuateSolenoidInAndOut.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void actuateIn() {
+        actuateSolenoidInAndOut.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void setMotorSpeed(double speed) {
+        mainMotor.set(speed);
+    }
+
+}
