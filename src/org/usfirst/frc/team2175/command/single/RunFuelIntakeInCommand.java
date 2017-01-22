7dbb54ae5bc274ce1abcd6a6a359bb8a3174f49f
Noah Connors <noahconnors@mnmsa.org>
2017-01-21 19:54:45 -0600

    Add RunFuelIntakeinCommand
    This command will be used for taking in fuel during teleop on a button
    press

---
+++ src/org/usfirst/frc/team2175/command/single/RunFuelIntakeInCommand.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team2175.command.single;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.BaseCommand;
+import org.usfirst.frc.team2175.subsystem.FuelIntakeSubsystem;
+
+public class RunFuelIntakeInCommand extends BaseCommand {
+    FuelIntakeSubsystem fuelIntakeSubsystem;
+
+    public RunFuelIntakeInCommand() {
+        super();
+        fuelIntakeSubsystem = ServiceLocator.get(FuelIntakeSubsystem.class);
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        // TODO get speed value from properties file
+        fuelIntakeSubsystem.setMotorSpeed(1);
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+        super.end();
+        fuelIntakeSubsystem.setMotorSpeed(0);
+    }
+
+    @Override
+    protected void interrupted() {
+    }
+}
