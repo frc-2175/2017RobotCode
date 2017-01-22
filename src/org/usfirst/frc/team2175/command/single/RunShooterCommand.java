37070f7730ea7f40de3e235a428fbf15cbd36775
Adisesh Viruru <adisesh.viruru@gmail.com>
2017-01-21 21:01:47 -0600

    add the shotter command again

--- src/org/usfirst/frc/team2175/command/single/RunShooterCommand.java
+++ src/org/usfirst/frc/team2175/command/single/RunShooterCommand.java
@@ -3,44 +3,39 @@ package org.usfirst.frc.team2175.command.single;
 import org.usfirst.frc.team2175.command.BaseCommand;
 import org.usfirst.frc.team2175.subsystem.ShooterSubsystem;
 
-public class RunShooterCommand extends BaseCommand
-{
+public class RunShooterCommand extends BaseCommand {
     private final ShooterSubsystem shooterSubsystem;
     double initialShooterSpeed = 1.0;
     double initialFeederSpeed = 0.8;
     double initialAgitatorSpeed = 0.5;
 
     public RunShooterCommand() {
-	shooterSubsystem = new ShooterSubsystem();
+        shooterSubsystem = new ShooterSubsystem();
     }
 
     @Override
-    protected void initialize()
-    {
+    protected void initialize() {
 
-	shooterSubsystem.setMotorSpeed(initialShooterSpeed, initialFeederSpeed, initialAgitatorSpeed);
+        shooterSubsystem.setMotorSpeed(initialShooterSpeed, initialFeederSpeed,
+                initialAgitatorSpeed);
 
     }
 
     @Override
-    protected void execute()
-    {
+    protected void execute() {
     }
 
     @Override
-    protected boolean isFinished()
-    {
-	return true;
+    protected boolean isFinished() {
+        return true;
     }
 
     @Override
-    protected void end()
-    {
+    protected void end() {
     }
 
     @Override
-    protected void interrupted()
-    {
+    protected void interrupted() {
     }
 
 }
