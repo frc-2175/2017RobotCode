683c2708006cc4bcfd759bfd92c1ba4c00d5c9f0
2175 <info@fightingcalculators.org>
2017-01-22 14:11:28 -0600

    Add batch files for copying properties files (requires putty)

---
+++ deployPropertiesFiles_Competition.bat
@@ -0,0 +1,1 @@
+pscp -pw "" src\properties\competitionbot\*.properties admin@roborio-2175-frc.local:/home/lvuser
---
+++ deployPropertiesFiles_Practice.bat
@@ -0,0 +1,1 @@
+pscp -pw "" src\properties\practicebot\*.properties admin@roborio-2175-frc.local:/home/lvuser
