7ccdae9a30cbef85ba31e8e88a5b1314eb0fa0de
Noah Connors <noahconnors@mnmsa.org>
2017-01-23 20:55:44 -0600

    Add ClimberSubsystem

---
+++ src/org/usfirst/frc/team2175/subsystem/ClimberSubsystem.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team2175.subsystem;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.properties.WiringProperties;
+
+import com.ctre.CANTalon;
+
+public class ClimberSubsystem extends BaseSubsystem {
+    CANTalon mainMotor;
+
+    public ClimberSubsystem() {
+        WiringProperties wiringProperties =
+                ServiceLocator.get(WiringProperties.class);
+        mainMotor = new CANTalon(wiringProperties.getClimberMainMotorNumber());
+    }
+
+    public void setClimberSpeed(double speed) {
+        mainMotor.set(speed);
+    }
+}
