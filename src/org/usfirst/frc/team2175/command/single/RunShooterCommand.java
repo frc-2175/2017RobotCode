6bca4f480a283ad67a66564a14d765d3eec599ea
Adisesh Viruru <adisesh.viruru@gmail.com>
2017-01-23 19:53:50 -0600

    fixes mistakes such as no end .

--- src/org/usfirst/frc/team2175/command/single/RunShooterCommand.java
+++ src/org/usfirst/frc/team2175/command/single/RunShooterCommand.java
@@ -1,24 +1,36 @@
 package org.usfirst.frc.team2175.command.single;
 
+import org.usfirst.frc.team2175.ServiceLocator;
 import org.usfirst.frc.team2175.command.BaseCommand;
 import org.usfirst.frc.team2175.subsystem.ShooterSubsystem;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class RunShooterCommand extends BaseCommand {
     private final ShooterSubsystem shooterSubsystem;
-    double initialShooterSpeed = 1.0;
-    double initialFeederSpeed = 0.8;
-    double initialAgitatorSpeed = 0.5;
+    final double initialShooterSpeed = 1.0;
+    final double initialFeederSpeed = 0.8;
+    final double initialAgitatorSpeed = 0.5;
+    Joystick gamepad;
 
     public RunShooterCommand() {
-        shooterSubsystem = new ShooterSubsystem();
+        shooterSubsystem = ServiceLocator.get(ShooterSubsystem.class);
     }
 
     @Override
     protected void initialize() {
 
-        shooterSubsystem.setMotorSpeed(initialShooterSpeed, initialFeederSpeed,
-                initialAgitatorSpeed);
+        if (gamepad.getRawButton(7)) {
+            shooterSubsystem.setMotorSpeed(initialShooterSpeed);
+        } else if (gamepad.getRawButton(8)) {
+            shooterSubsystem.setMotorSpeedFeeder(initialFeederSpeed);
+            shooterSubsystem.setMotorSpeedAgitator(initialAgitatorSpeed);
+        }
 
+        else {
+            shooterSubsystem.setMotorSpeedFeeder(0);
+            shooterSubsystem.setMotorSpeedAgitator(0);
+        }
     }
 
     @Override
@@ -27,11 +39,14 @@ public class RunShooterCommand extends BaseCommand {
 
     @Override
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     @Override
     protected void end() {
+        shooterSubsystem.setMotorSpeed(0);
+        shooterSubsystem.setMotorSpeedFeeder(0);
+        shooterSubsystem.setMotorSpeedAgitator(0);
     }
 
     @Override
