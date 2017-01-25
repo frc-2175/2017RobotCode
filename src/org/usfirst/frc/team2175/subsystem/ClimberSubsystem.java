cf2ea625301e0476d741ec57c6ec7ac66dc007c6
Noah Connors <noahconnors@mnmsa.org>
2017-01-25 17:37:53 -0600

    Added behavior properties to subsystems

--- src/org/usfirst/frc/team2175/subsystem/ClimberSubsystem.java
+++ src/org/usfirst/frc/team2175/subsystem/ClimberSubsystem.java
@@ -1,21 +1,31 @@
 package org.usfirst.frc.team2175.subsystem;
 
 import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.properties.BehaviorProperties;
 import org.usfirst.frc.team2175.properties.WiringProperties;
 
 import com.ctre.CANTalon;
 
 public class ClimberSubsystem extends BaseSubsystem {
     private final CANTalon mainMotor;
+    private final double mainMotorDefaultSpeed;
 
     public ClimberSubsystem() {
         super();
         WiringProperties wiringProperties =
                 ServiceLocator.get(WiringProperties.class);
+        BehaviorProperties behaviorProperties =
+                ServiceLocator.get(BehaviorProperties.class);
         mainMotor = new CANTalon(wiringProperties.getClimberMainMotorNumber());
+        mainMotorDefaultSpeed = behaviorProperties.getClimberMasterSpeed();
     }
 
     public void setClimberSpeed(double speed) {
         mainMotor.set(speed);
     }
+
+    public double getMainMotorDefaultSpeed() {
+        return mainMotorDefaultSpeed;
+    }
+
 }
--- src/org/usfirst/frc/team2175/subsystem/FuelIntakeSubsystem.java
+++ src/org/usfirst/frc/team2175/subsystem/FuelIntakeSubsystem.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team2175.subsystem;
 
 import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.properties.BehaviorProperties;
 import org.usfirst.frc.team2175.properties.WiringProperties;
 
 import com.ctre.CANTalon;
@@ -10,15 +11,29 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 public class FuelIntakeSubsystem extends BaseSubsystem {
     private final CANTalon mainMotor;
     private final DoubleSolenoid actuateSolenoidInAndOut;
+    private final double mainMotorDefaultInSpeed;
+    private final double mainMotorDefaultOutSpeed;
 
     public FuelIntakeSubsystem() {
         WiringProperties wiringProperties =
                 ServiceLocator.get(WiringProperties.class);
+        BehaviorProperties behaviorProperties =
+                ServiceLocator.get(BehaviorProperties.class);
         mainMotor = new CANTalon(
                 wiringProperties.getFuelIntakeMainMotorDeviceNumber());
         actuateSolenoidInAndOut = new DoubleSolenoid(
                 wiringProperties.getFuelIntakeActuaterForwardNumber(),
                 wiringProperties.getFuelIntakeActuaterReverseNumber());
+        mainMotorDefaultInSpeed = behaviorProperties.getFuelIntakeInSpeed();
+        mainMotorDefaultOutSpeed = behaviorProperties.getFuelIntakeOutSpeed();
+    }
+
+    public double getMainMotorDefaultInSpeed() {
+        return mainMotorDefaultInSpeed;
+    }
+
+    public double getMainMotorDefaultOutSpeed() {
+        return mainMotorDefaultOutSpeed;
     }
 
     public void actuateOut() {
