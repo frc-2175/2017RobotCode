f1c6f5f8b61db57926a3e97779e21189323805b6
Noah Connors <noahconnors@mnmsa.org>
2017-01-26 21:22:08 -0600

    Add RunFuelIntakeOutCommand

---
+++ src/org/usfirst/frc/team2175/command/single/RunFuelIntakeOutCommand.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team2175.command.single;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.BaseCommand;
+import org.usfirst.frc.team2175.subsystem.FuelIntakeSubsystem;
+
+public class RunFuelIntakeOutCommand extends BaseCommand {
+    private final FuelIntakeSubsystem fuelIntakeSubsystem;
+
+    public RunFuelIntakeOutCommand() {
+        fuelIntakeSubsystem = ServiceLocator.get(FuelIntakeSubsystem.class);
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        fuelIntakeSubsystem.setMotorSpeed(
+                fuelIntakeSubsystem.getMainMotorDefaultOutSpeed());
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+        super.end();
+        fuelIntakeSubsystem.setMotorSpeed(0);
+    }
+}
