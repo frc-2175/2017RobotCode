198e7077b3ffed433859281596b7b68d4a3c8509
Ben Visness <bvisness@gmail.com>
2017-01-28 15:51:17 -0600

    Put log files in ~/log directory on roboRIO

--- deployPropertiesFiles_Practice.bat
+++ deployPropertiesFiles_Practice.bat
@@ -1,2 +1,2 @@
 pscp -pw "" src\properties\practicebot\*.properties admin@roborio-2175-frc.local:/home/lvuser
-plink -ssh -pw "" admin@roborio-2175-frc.local "killall -q netconsole-host || :"
+plink -ssh -pw "" admin@roborio-2175-frc.local "mkdir -m 775 -v log; killall -q netconsole-host || :"
--- deployPropertiesFiles_Practice.sh
+++ deployPropertiesFiles_Practice.sh
@@ -6,4 +6,4 @@ do
 	c="$c $f"
 done
 scp $c admin@roborio-9001-frc.local:/home/lvuser
-ssh admin@roborio-9001-frc.local "killall -q netconsole-host || :"
+ssh admin@roborio-9001-frc.local "mkdir -m 775 -v log; killall -q netconsole-host || :"
