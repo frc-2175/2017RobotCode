a810c3deb486ef3f5d2dfa7c199e2f7fdcd1b440
Noah Connors <noahconnors@mnmsa.org>
2017-02-01 17:58:58 -0600

    Add VisionSubsystem for vision processing with network tables

---
+++ src/org/usfirst/frc/team2175/subsystem/VisionSubsystem.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc.team2175.subsystem;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class VisionSubsystem extends BaseSubsystem {
+    private final NetworkTable table;
+    private double[] contourArea;
+    private double[] contourCenterX;
+    private double[] contourCenterY;
+    private double[] contourHeight;
+    private double[] contourWidth;
+    private double[] contourSolidity;
+    private double[] defaultValue;
+
+    public VisionSubsystem() {
+        table = NetworkTable.getTable("Grip/myCountoursReport");
+    }
+
+    public void updateAllValues() {
+        synchronized (table) {
+            contourArea = table.getNumberArray("area", defaultValue);
+            contourCenterX = table.getNumberArray("centerX", defaultValue);
+            contourCenterY = table.getNumberArray("centerY", defaultValue);
+            contourHeight = table.getNumberArray("height", defaultValue);
+            contourWidth = table.getNumberArray("width", defaultValue);
+            contourSolidity = table.getNumberArray("solidity", defaultValue);
+        }
+    }
+
+    public double[] getContourArea() {
+        return contourArea;
+    }
+
+    public double[] getContourCenterX() {
+        return contourCenterX;
+    }
+
+    public double[] getContourCenterY() {
+        return contourCenterY;
+    }
+
+    public double[] getContourHeight() {
+        return contourHeight;
+    }
+
+    public double[] getContourWidth() {
+        return contourWidth;
+    }
+
+    public double[] getContourSolidity() {
+        return contourSolidity;
+    }
+
+    public double calculateDistanceFromRetroTape() {
+        double distanceFromTape;
+        updateAllValues();
+        distanceFromTape = 0;
+        return distanceFromTape;
+    }
+
+}
