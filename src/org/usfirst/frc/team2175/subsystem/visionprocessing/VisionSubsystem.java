89af59086cad8c8b5d99982bb97700c33267a04a
Kevin La Casse <wolfklacasse@gmail.com>
2017-02-04 16:23:44 -0600

    Comment out Network Table stuff

--- src/org/usfirst/frc/team2175/subsystem/visionprocessing/VisionSubsystem.java
+++ src/org/usfirst/frc/team2175/subsystem/visionprocessing/VisionSubsystem.java
@@ -2,10 +2,8 @@ package org.usfirst.frc.team2175.subsystem.visionprocessing;
 
 import org.usfirst.frc.team2175.subsystem.BaseSubsystem;
 
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
-
 public class VisionSubsystem extends BaseSubsystem {
-    private final NetworkTable table;
+    // private final NetworkTable table;
     private double[] contourArea;
     private double[] contourCenterX;
     private double[] contourCenterY;
@@ -14,20 +12,20 @@ public class VisionSubsystem extends BaseSubsystem {
     private double[] contourSolidity;
     private double[] defaultValue;
 
-    public VisionSubsystem() {
-        table = NetworkTable.getTable("Grip/myCountoursReport");
-    }
-
-    public void updateAllValues() {
-        synchronized (table) {
-            contourArea = table.getNumberArray("area", defaultValue);
-            contourCenterX = table.getNumberArray("centerX", defaultValue);
-            contourCenterY = table.getNumberArray("centerY", defaultValue);
-            contourHeight = table.getNumberArray("height", defaultValue);
-            contourWidth = table.getNumberArray("width", defaultValue);
-            contourSolidity = table.getNumberArray("solidity", defaultValue);
-        }
-    }
+    // public VisionSubsystem() {
+    // table = NetworkTable.getTable("Grip/myCountoursReport");
+    // }
+    //
+    // public void updateAllValues() {
+    // synchronized (table) {
+    // contourArea = table.getNumberArray("area", defaultValue);
+    // contourCenterX = table.getNumberArray("centerX", defaultValue);
+    // contourCenterY = table.getNumberArray("centerY", defaultValue);
+    // contourHeight = table.getNumberArray("height", defaultValue);
+    // contourWidth = table.getNumberArray("width", defaultValue);
+    // contourSolidity = table.getNumberArray("solidity", defaultValue);
+    // }
+    // }
 
     public double[] getContourArea() {
         return contourArea;
@@ -55,7 +53,7 @@ public class VisionSubsystem extends BaseSubsystem {
 
     public double calculateDistanceFromRetroTape() {
         double distanceFromTape;
-        updateAllValues();
+        // updateAllValues();
         distanceFromTape = 0;
         return distanceFromTape;
     }
