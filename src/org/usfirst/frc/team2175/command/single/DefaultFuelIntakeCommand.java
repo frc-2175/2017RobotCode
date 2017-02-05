d02c1db8474ae26a88cc25345a5e0a3633d19ef5
Noah Connors <noahconnors@mnmsa.org>
2017-02-04 22:06:05 -0600

    Add fuel intake default command

---
+++ src/org/usfirst/frc/team2175/command/single/DefaultFuelIntakeCommand.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team2175.command.single;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.BaseCommand;
+import org.usfirst.frc.team2175.driverstation.DriverStation;
+import org.usfirst.frc.team2175.subsystem.FuelIntakeSubsystem;
+
+public class DefaultFuelIntakeCommand extends BaseCommand {
+    private final FuelIntakeSubsystem fuelIntakeSubsystem;
+    private final DriverStation driverStation;
+
+    public DefaultFuelIntakeCommand() {
+        fuelIntakeSubsystem = ServiceLocator.get(FuelIntakeSubsystem.class);
+        driverStation = ServiceLocator.get(DriverStation.class);
+    }
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+        if (driverStation.getRightPOVIsPressed()) {
+            fuelIntakeSubsystem.setMotorSpeed(
+                    fuelIntakeSubsystem.getMainMotorDefaultOutSpeed());
+        } else {
+            fuelIntakeSubsystem.setMotorSpeed(0);
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+        fuelIntakeSubsystem.setMotorSpeed(0);
+    }
+}
