2ce8a43e1341aafa7417b45382ea86b33d2dd313
Noah Connors <noahconnors@mnmsa.org>
2017-02-04 22:05:48 -0600

    Add shooter default command

---
+++ src/org/usfirst/frc/team2175/command/single/DefaultShooterCommand.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team2175.command.single;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.BaseCommand;
+import org.usfirst.frc.team2175.driverstation.DriverStation;
+import org.usfirst.frc.team2175.subsystem.ShooterSubsystem;
+
+public class DefaultShooterCommand extends BaseCommand {
+    private final ShooterSubsystem shooterSubsystem;
+    private final DriverStation driverStation;
+
+    public DefaultShooterCommand() {
+        shooterSubsystem = ServiceLocator.get(ShooterSubsystem.class);
+        driverStation = ServiceLocator.get(DriverStation.class);
+    }
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+        if (driverStation.getDownPOVIsPressed()) {
+            shooterSubsystem.setShooterReverseSpeed();
+        } else {
+            shooterSubsystem.setShooterSpeedZero();
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+        shooterSubsystem.setShooterSpeedZero();
+    }
+}
