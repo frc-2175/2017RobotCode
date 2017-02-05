33cce73103c82ace91c5eae179ff5961794e903c
Noah Connors <noahconnors@mnmsa.org>
2017-02-04 19:24:08 -0600

    Make second climber motor slave of first
    these motors ALLWAYS need to spin the same way

--- src/org/usfirst/frc/team2175/subsystem/ClimberSubsystem.java
+++ src/org/usfirst/frc/team2175/subsystem/ClimberSubsystem.java
@@ -5,6 +5,7 @@ import org.usfirst.frc.team2175.properties.BehaviorProperties;
 import org.usfirst.frc.team2175.properties.WiringProperties;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 
 public class ClimberSubsystem extends BaseSubsystem {
     private final CANTalon motorOne;
@@ -13,22 +14,22 @@ public class ClimberSubsystem extends BaseSubsystem {
 
     public ClimberSubsystem() {
         super();
-        WiringProperties wiringProperties =
+        final WiringProperties wiringProperties =
                 ServiceLocator.get(WiringProperties.class);
-        BehaviorProperties behaviorProperties =
+        final BehaviorProperties behaviorProperties =
                 ServiceLocator.get(BehaviorProperties.class);
         motorOne = new CANTalon(wiringProperties.getClimberMotorOneNumber());
         motorTwo = new CANTalon(wiringProperties.getClimberMotorTwoNumber());
+        motorTwo.changeControlMode(TalonControlMode.Follower);
+        motorTwo.set(motorOne.getDeviceID());
         mainMotorDefaultSpeed = behaviorProperties.getClimberMasterSpeed();
     }
 
-    public void setClimberSpeed(double speed) {
+    public void setClimberSpeed(final double speed) {
         motorOne.set(speed);
-        motorTwo.set(speed);
     }
 
     public double getMainMotorDefaultSpeed() {
         return mainMotorDefaultSpeed;
     }
-
 }
