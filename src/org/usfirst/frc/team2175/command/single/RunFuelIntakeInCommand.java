335d262c50da13718f61cac59c666f8ed0c294c1
Kevin La Casse <wolfklacasse@gmail.com>
2017-02-12 14:39:21 -0600

    Add agitator to fuel intake command

--- src/org/usfirst/frc/team2175/command/single/RunFuelIntakeInCommand.java
+++ src/org/usfirst/frc/team2175/command/single/RunFuelIntakeInCommand.java
@@ -3,15 +3,19 @@ package org.usfirst.frc.team2175.command.single;
 import org.usfirst.frc.team2175.ServiceLocator;
 import org.usfirst.frc.team2175.command.BaseCommand;
 import org.usfirst.frc.team2175.subsystem.FuelIntakeSubsystem;
+import org.usfirst.frc.team2175.subsystem.ShooterSubsystem;
 
 public class RunFuelIntakeInCommand extends BaseCommand {
 
     private final FuelIntakeSubsystem fuelIntakeSubsystem;
+    private ShooterSubsystem shooterSubsystem;
 
     public RunFuelIntakeInCommand() {
         fuelIntakeSubsystem = ServiceLocator.get(FuelIntakeSubsystem.class);
+        shooterSubsystem = ServiceLocator.get(ShooterSubsystem.class);
 
         requires(fuelIntakeSubsystem);
+        requires(shooterSubsystem);
     }
 
     @Override
@@ -19,6 +23,7 @@ public class RunFuelIntakeInCommand extends BaseCommand {
         super.initialize();
         fuelIntakeSubsystem.setMotorSpeed(
                 fuelIntakeSubsystem.getMainMotorDefaultInSpeed());
+        shooterSubsystem.setAgitatorDefaultSpeed();
     }
 
     @Override
@@ -34,5 +39,6 @@ public class RunFuelIntakeInCommand extends BaseCommand {
     protected void end() {
         super.end();
         fuelIntakeSubsystem.setMotorSpeed(0);
+        shooterSubsystem.setAgitatorSpeedZero();
     }
 }
