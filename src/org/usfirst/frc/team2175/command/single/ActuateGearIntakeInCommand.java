d117aa651eff08dd4e8f7505a90aafaeebe89b2a
Noah Connors <noahconnors@mnmsa.org>
2017-02-11 18:25:42 -0600

    Add gear intake actuation commands for out and in

---
+++ src/org/usfirst/frc/team2175/command/single/ActuateGearIntakeInCommand.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team2175.command.single;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.BaseCommand;
+import org.usfirst.frc.team2175.subsystem.GearIntakeSubsystem;
+
+public class ActuateGearIntakeInCommand extends BaseCommand {
+    GearIntakeSubsystem gearIntakeSubsystem;
+
+    public ActuateGearIntakeInCommand() {
+        gearIntakeSubsystem = ServiceLocator.get(GearIntakeSubsystem.class);
+        requires(gearIntakeSubsystem);
+    }
+
+    @Override
+    protected void initialize() {
+        gearIntakeSubsystem.raiseIntake();
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    protected void end() {
+    }
+}
---
+++ src/org/usfirst/frc/team2175/command/single/ActuateGearIntakeOutCommand.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team2175.command.single;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.BaseCommand;
+import org.usfirst.frc.team2175.subsystem.GearIntakeSubsystem;
+
+public class ActuateGearIntakeOutCommand extends BaseCommand {
+    GearIntakeSubsystem gearIntakeSubsystem;
+
+    public ActuateGearIntakeOutCommand() {
+        gearIntakeSubsystem = ServiceLocator.get(GearIntakeSubsystem.class);
+        requires(gearIntakeSubsystem);
+    }
+
+    @Override
+    protected void initialize() {
+        gearIntakeSubsystem.lowerIntake();
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    protected void end() {
+    }
+}
