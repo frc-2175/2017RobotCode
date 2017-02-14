81e2f4b516ab3c5c8c71d0bcd6ec5b8b5bdbe47d
Ben Visness <bvisness@gmail.com>
2017-02-13 21:37:53 -0600

    Suppress warnings when making the log directory

--- deployPropertiesFiles_Practice.bat
+++ deployPropertiesFiles_Practice.bat
@@ -3,4 +3,4 @@ pscp -pw "" src\properties\*.properties admin@roborio-2175-frc.local:/home/lvuse
 ::Bot Specified Files
 pscp -pw "" src\properties\practicebot\*.properties admin@roborio-2175-frc.local:/home/lvuser
 ::SSH files to robot
-plink -ssh -pw "" admin@roborio-2175-frc.local "mkdir -m 775 -v log; killall -q netconsole-host || :"
+plink -ssh -pw "" admin@roborio-2175-frc.local "mkdir -m 775 -v -p log; killall -q netconsole-host || :"
--- deployPropertiesFiles_Practice.sh
+++ deployPropertiesFiles_Practice.sh
@@ -3,4 +3,4 @@ scp src\properties\*.properties admin@roborio-2175-frc.local:/home/lvuser
 # Bot Specified Files
 scp src\properties\practicebot\*.properties admin@roborio-2175-frc.local:/home/lvuser
 # SSH files to robot
-ssh admin@roborio-2175-frc.local "mkdir -m 775 -v log; killall -q netconsole-host || :"
+ssh admin@roborio-2175-frc.local "mkdir -m 775 -v -p log; killall -q netconsole-host || :"
