5ffbb790156fdf778cf36412e30cdafe990242ae
Noah Connors <noahconnors@mnmsa.org>
2017-02-17 16:48:12 -0600

    Add a command group for driving past the baseline in auton

---
+++ src/org/usfirst/frc/team2175/command/group/DriveForwardToCrossBaselineCommandGroup.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2175.command.group;
+
+import org.usfirst.frc.team2175.command.single.DriveInchesCommand;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class DriveForwardToCrossBaselineCommandGroup extends CommandGroup {
+    public DriveForwardToCrossBaselineCommandGroup() {
+        addSequential(new DriveInchesCommand(93.0));
+    }
+}
