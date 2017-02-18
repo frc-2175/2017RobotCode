44093b8e755968548ffdc4916f4c2912b1ac063a
Noah Connors <noahconnors@mnmsa.org>
2017-02-18 08:27:58 -0600

    Create time based auton to cross baseling

---
+++ src/org/usfirst/frc/team2175/command/autonomous/CrossBaselineTimeBasedAutonomous.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2175.command.autonomous;
+
+import org.usfirst.frc.team2175.command.single.ArcadeDriveForAutonCommand;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class CrossBaselineTimeBasedAutonomous extends CommandGroup {
+    public CrossBaselineTimeBasedAutonomous() {
+        addSequential(new ArcadeDriveForAutonCommand(0.5, 0), 5);
+    }
+}
