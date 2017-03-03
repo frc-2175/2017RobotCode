9f96084ae43dce02135a5a2b3e0df4ef348b7828
Darmok <info@fightingcalculators.org>
2017-03-02 20:57:49 -0600

    Get Network Tables for vision

--- src/org/usfirst/frc/team2175/subsystem/visionprocessing/VisionSubsystem.java
+++ src/org/usfirst/frc/team2175/subsystem/visionprocessing/VisionSubsystem.java
@@ -1,22 +1,14 @@
 package org.usfirst.frc.team2175.subsystem.visionprocessing;
 
-import java.util.ArrayList;
-
-import org.opencv.core.MatOfPoint;
-import org.usfirst.frc.team2175.ServiceLocator;
 import org.usfirst.frc.team2175.subsystem.BaseSubsystem;
 
 import edu.wpi.cscore.CvSource;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.vision.VisionRunner;
 
-public class VisionSubsystem extends BaseSubsystem
-        implements VisionRunner.Listener<GripPipeline> {
+public class VisionSubsystem extends BaseSubsystem {
 
     private CvSource processOutput;
-    private NetworkTable table;
+    // private NetworkTable table;
     private double[] contourArea;
     private double[] contourCenterX;
     private double[] contourCenterY;
@@ -29,12 +21,10 @@ public class VisionSubsystem extends BaseSubsystem
     public VisionSubsystem() {
         startAutomaticCapture();
         // startGripPipelineCapture();
-        table = NetworkTable.getTable("gearReport");
-        ServiceLocator.register(this);
     }
 
     private void startAutomaticCapture() {
-        CameraServer.getInstance().startAutomaticCapture();
+        CameraServer.getInstance().startAutomaticCapture(0);
     }
 
     // private void startGripPipelineCapture() {
@@ -49,14 +39,6 @@ public class VisionSubsystem extends BaseSubsystem
     // processOutput =
     // CameraServer.getInstance().putVideo("Process Output", 320, 240);
     // }
-
-    @Override
-    public void copyPipelineOutputs(final GripPipeline pipeline) {
-        processOutput.putFrame(pipeline.hsvThresholdOutput());
-
-        final ArrayList<MatOfPoint> contours = pipeline.filterContoursOutput();
-        SmartDashboard.putNumber("Contour Count", contours.size());
-    }
 
     public double[] getContourArea() {
         return contourArea;
@@ -80,9 +62,5 @@ public class VisionSubsystem extends BaseSubsystem
 
     public double[] getContourSolidity() {
         return contourSolidity;
-    }
-
-    public double getDegreesToTurnToGear() {
-        return table.getNumber("degreesToTurn", 0);
     }
 }
