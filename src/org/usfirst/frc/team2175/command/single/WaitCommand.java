d16d18bb3c2d1bd2aa62c6b836408c394da76685
Darmok <info@fightingcalculators.org>
2017-03-03 18:04:58 -0600

    Add a wait command for later use

---
+++ src/org/usfirst/frc/team2175/command/single/WaitCommand.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team2175.command.single;
+
+import org.usfirst.frc.team2175.command.BaseCommand;
+
+public class WaitCommand extends BaseCommand {
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
