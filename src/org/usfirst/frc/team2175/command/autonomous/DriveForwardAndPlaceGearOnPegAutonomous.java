8b5d3d1e1d3922fe4c416d609acd81b47e305e1c
Darmok <info@fightingcalculators.org>
2017-03-03 18:03:51 -0600

    Update Middle Peg Auton

--- src/org/usfirst/frc/team2175/command/autonomous/DriveForwardAndPlaceGearOnPegAutonomous.java
+++ src/org/usfirst/frc/team2175/command/autonomous/DriveForwardAndPlaceGearOnPegAutonomous.java
@@ -8,8 +8,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DriveForwardAndPlaceGearOnPegAutonomous extends CommandGroup {
     public DriveForwardAndPlaceGearOnPegAutonomous() {
-        addSequential(new ArcadeDriveForAutonCommand(-0.7, 0), 4);
-        addSequential(new ActuateGearIntakeOutAndSpinCommand(), .8);
+        addSequential(new ArcadeDriveForAutonCommand(-0.6, 0), 4);
+        addSequential(new ActuateGearIntakeOutAndSpinCommand(), 1.8);
         addSequential(new ArcadeDriveForAutonCommand(0.7, 0), 1);
         addSequential(new ActuateGearIntakeInCommand());
     }
