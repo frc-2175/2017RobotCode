be59b199d04ab46f644e0125b03ad5f9e31c3b63
Forthwind <wolfklacasse@gmail.com>
2017-03-14 21:49:58 -0500

    Separate items SmartDashboard shows
    Simplify code

--- src/org/usfirst/frc/team2175/loop/SmartDashboardLoop.java
+++ src/org/usfirst/frc/team2175/loop/SmartDashboardLoop.java
@@ -4,8 +4,9 @@ import org.usfirst.frc.team2175.ServiceLocator;
 import org.usfirst.frc.team2175.command.autonomous.CrossBaselineTimeBasedAutonomous;
 import org.usfirst.frc.team2175.command.autonomous.DoNothingAutonomous;
 import org.usfirst.frc.team2175.command.autonomous.DriveForwardAndPlaceGearOnPegAutonomous;
-import org.usfirst.frc.team2175.command.autonomous.DriveOneFootForwardSimpleAutonomous;
+import org.usfirst.frc.team2175.command.autonomous.RightPegAutonomous;
 import org.usfirst.frc.team2175.command.autonomous.TurnDegreesFromUDP;
+import org.usfirst.frc.team2175.subsystem.ClimberSubsystem;
 import org.usfirst.frc.team2175.subsystem.FuelIntakeSubsystem;
 import org.usfirst.frc.team2175.subsystem.GearIntakeSubsystem;
 import org.usfirst.frc.team2175.subsystem.communications.CommunicationSubsystem;
@@ -17,11 +18,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SmartDashboardLoop extends ControlLoop {
     private final GearIntakeSubsystem gearIntakeSubsystem;
+    private final DrivetrainSubsystem drivetrainSubsystem;
 
-    private final SendableChooser<Command> autonSelector;
+    private SendableChooser<Command> autonSelector;
 
     public SmartDashboardLoop() {
         gearIntakeSubsystem = ServiceLocator.get(GearIntakeSubsystem.class);
+        drivetrainSubsystem = ServiceLocator.get(DrivetrainSubsystem.class);
 
         autonSelector = new SendableChooser<>();
 
@@ -36,35 +39,6 @@ public class SmartDashboardLoop extends ControlLoop {
         return 10;
     }
 
-    @Override
-    protected void runTask() {
-        showGearIntakeInfo();
-        SmartDashboard.putNumber("CurrentDraw", ServiceLocator
-                .get(DrivetrainSubsystem.class).getOutputCurrent());
-        SmartDashboard.putNumber("Gyro",
-                ServiceLocator.get(DrivetrainSubsystem.class).getGyroAngle());
-        SmartDashboard.putNumber("GyroGraph",
-                ServiceLocator.get(DrivetrainSubsystem.class).getGyroAngle());
-        SmartDashboard.putBoolean("GearActuatorOut", ServiceLocator
-                .get(GearIntakeSubsystem.class).getIsGearIntakeOut());
-        SmartDashboard.putNumber("Encoder", ServiceLocator
-                .get(DrivetrainSubsystem.class).getLeftEncoderDistance());
-        SmartDashboard.putBoolean("Is Hopper Up",
-                ServiceLocator.get(FuelIntakeSubsystem.class).isHopperUp());
-        SmartDashboard.putNumber("Right Encoder", ServiceLocator
-                .get(DrivetrainSubsystem.class).getRightEncoderDistance());
-
-        SmartDashboard.putNumber("GyroVal",
-                ServiceLocator.get(CommunicationSubsystem.class).getSetpoint());
-    }
-
-    protected void showGearIntakeInfo() {
-        SmartDashboard.putNumber("Current",
-                gearIntakeSubsystem.getLeftMotorCurrent());
-        SmartDashboard.putBoolean("Gear in Gear Intake",
-                gearIntakeSubsystem.isCancelled());
-    }
-
     private void populateAutonSelector() {
         autonSelector.addObject("No Auton", new DoNothingAutonomous());
         autonSelector.addObject("Cross Baseline",
@@ -74,7 +48,41 @@ public class SmartDashboardLoop extends ControlLoop {
         autonSelector.addDefault("Turn Degrees From UDP",
                 new TurnDegreesFromUDP());
         autonSelector.addObject("Drive Two Feet Forward",
-                new DriveOneFootForwardSimpleAutonomous());
+                new RightPegAutonomous());
+    }
+
+    @Override
+    protected void runTask() {
+        showGearIntakeInfo();
+        showDrivetrainInfo();
+
+        SmartDashboard.putNumber("Gyro value from UDP",
+                ServiceLocator.get(CommunicationSubsystem.class).getSetpoint());
+        SmartDashboard.putNumber("Current Output",
+                ServiceLocator.get(ClimberSubsystem.class).getOutputCurrent());
+    }
+
+    protected void showGearIntakeInfo() {
+        SmartDashboard.putNumber("Current",
+                gearIntakeSubsystem.getLeftMotorCurrent());
+        SmartDashboard.putBoolean("Gear in Gear Intake",
+                gearIntakeSubsystem.isCancelled());
+        SmartDashboard.putBoolean("GearActuatorOut",
+                gearIntakeSubsystem.getIsGearIntakeOut());
+    }
+
+    protected void showDrivetrainInfo() {
+        SmartDashboard.putNumber("CurrentDraw",
+                drivetrainSubsystem.getOutputCurrent());
+        SmartDashboard.putNumber("Gyro", drivetrainSubsystem.getGyroAngle());
+        SmartDashboard.putNumber("GyroGraph",
+                drivetrainSubsystem.getGyroAngle());
+        SmartDashboard.putNumber("Encoder",
+                drivetrainSubsystem.getLeftEncoderDistance());
+        SmartDashboard.putBoolean("Is Hopper Up",
+                ServiceLocator.get(FuelIntakeSubsystem.class).isHopperUp());
+        SmartDashboard.putNumber("Right Encoder",
+                drivetrainSubsystem.getRightEncoderDistance());
     }
 
     public Command getAuton() {
