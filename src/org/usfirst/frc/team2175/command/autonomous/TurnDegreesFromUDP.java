2870f8c411a8b0b29ed6e4ad10d7ff96606905c2
Kevin La Casse <wolfklacasse@gmail.com>
2017-03-15 14:00:09 -0500

    Make turn degrees from udp turn degrees specified by udp

--- src/org/usfirst/frc/team2175/command/autonomous/TurnDegreesFromUDP.java
+++ src/org/usfirst/frc/team2175/command/autonomous/TurnDegreesFromUDP.java
@@ -1,19 +1,30 @@
 package org.usfirst.frc.team2175.command.autonomous;
 
 import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.BaseCommand;
 import org.usfirst.frc.team2175.command.single.TurnDegreesWithGyroCommand;
 import org.usfirst.frc.team2175.subsystem.communications.CommunicationSubsystem;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.Command;
 
-public class TurnDegreesFromUDP extends CommandGroup {
+public class TurnDegreesFromUDP extends BaseCommand {
     private CommunicationSubsystem communicationSubsystem;
 
     public TurnDegreesFromUDP() {
         communicationSubsystem =
                 ServiceLocator.get(CommunicationSubsystem.class);
-        addSequential(new TurnDegreesWithGyroCommand(
-                communicationSubsystem.getSetpoint()));
+    }
+
+    @Override
+    protected void initialize() {
+        final double setpoint = communicationSubsystem.getSetpoint();
+        final Command turnDegrees = new TurnDegreesWithGyroCommand(setpoint);
+        turnDegrees.start();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
     }
 
 }
